72f826df23c1e7eb79f4ecc589c9829a38020868
Aidan K. Ewart <aidanprattewart@gmail.com>
2020-05-27 18:31:13 +0100

    made basic ast

---
+++ AST.py
@@ -0,0 +1,15 @@
+class Instruction:
+    def __init__(self):
+        pass
+
+class Expr:
+    def __init__(self, ins):
+        if ins is list:
+            self.block = (i for i in ins)
+        elif ins is Instruction: # not sure if will work
+            self.ins = ins
+
+class Function:
+    def __init__(self, ident: str, body: Expr):
+        self.name = ident
+        self.body = body
